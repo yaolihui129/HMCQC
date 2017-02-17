5ea8be3f9a4d65363b0003fb717b9236280e631c
yaolihui129 <yaolihui129@sina.com>
2017-02-17 18:11:09 +0800

    修改部分信达的页面

--- Apps/Xinda/Controller/AboutController.class.php
+++ Apps/Xinda/Controller/AboutController.class.php
@@ -2,23 +2,38 @@
 namespace Xinda\Controller;
 use Think\Controller;
 class AboutController extends Controller {
-    public function index(){
+    public function about(){
+        $m=D('product');
+         $data=$m->field('web,adress,desc,phone,tel,qq,url,record,path,img')->find(6);
+         $_SESSION['Xinda']=$data;         
+         $_SESSION['Xinda']['img']=$data['path'].$data['img'];
+        // $_SESSION['Xinda']['web']=$data['web'];
+         $_SESSION['ip']=get_client_ip();
+         $_SESSION['browser']=GetBrowser();
+         $_SESSION['os']=GetOs();
+                       
+        $this->display();
+    }
+    
+    public function hr(){
+    
         $m=D('product');
         $data=$m->field('web,adress,desc,phone,tel,qq,url,record,path,img')->find(6);
         $_SESSION['Xinda']=$data;
         $_SESSION['Xinda']['img']=$data['path'].$data['img'];
+        $_SESSION['Xinda']['web']='北京智慧信达-招聘';
         $_SESSION['ip']=get_client_ip();
         $_SESSION['browser']=GetBrowser();
         $_SESSION['os']=GetOs();
-        
-        $m=M('tp_hr');
-        $where=array('state'=>"发布",'prodid'=>6);
-        $arr=$m->where($where)->select();
-        $this->assign('data',$arr);
-        
+    
+        $m=D('tp_hr');
+        $where['prodid']=6;
+        $where['state']='招聘中';
+        $data=$m->where($where)->order('sn,id')->select();
+        $this->assign('data',$data);
+    
         $this->display();
     }
-    
     
    
 }
