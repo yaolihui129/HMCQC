9ace6d260efcb942585c18f723ee41d9bb3b1a1c
yaolihui129 <yaolihui129@sina.com>
2017-02-22 18:33:11 +0800

    更新CSS

--- Apps/Anshun/Controller/AboutController.class.php
+++ Apps/Anshun/Controller/AboutController.class.php
@@ -2,7 +2,7 @@
 namespace Anshun\Controller;
 use Think\Controller;
 class AboutController extends Controller {
-    public function about(){
+    public function index(){
        
        $m=D('product');       
        $data=$m->field('web,adress,desc,phone,tel,qq,url,record,path,img')->find(4);
@@ -34,23 +34,5 @@ class AboutController extends Controller {
         $this->display();
     }
   
-    public function tech(){
-
-        $m=D('product');
-        $data=$m->field('web,adress,desc,phone,tel,qq,url,record,path,img')->find(4);
-        $_SESSION['Anshun']=$data;
-        $_SESSION['Anshun']['img']=$data['path'].$data['img'];
-        $_SESSION['ip']=get_client_ip();
-        $_SESSION['browser']=GetBrowser();
-        $_SESSION['os']=GetOs();
-    
-        
-        $m=M('tp_admin');
-        $where['tech']="技师";
-        $where['prodid']=4;
-        $data=$m->where($where)->select();
-        $this->assign('data',$data);
-              
-        $this->display();
-    }
+   
 }
